4bc3091e6d1b279c23e22ec447c656d8816be482
ethansworld <ethanalbaye@gmail.com>
2026-02-28 21:01:06 -0500

    Initial commit of End Portal Spawner Mod Java source file

---
+++ src/main/java/com/ethansworld/endportalspawnermod/EndPortalSpawnerMod.java
@@ -0,0 +1,13 @@
+// Main class for the mod
+package com.ethansworld.endportalspawnermod;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(EndPortalSpawnerMod.MODID)
+public class EndPortalSpawnerMod {
+    public static final String MODID = "endportalspawnermod";
+    
+    public EndPortalSpawnerMod() {
+        // Initialization code here
+    }
+}
